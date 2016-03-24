a0147e15f057471e3794ca3d0423ca683186e86e
george-hall <georgehall017@gmail.com>
2016-03-24 15:27:00 +0000

    Renamed program to GRIn (grin)

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-import calc_gri as calc_gri_main
+import grin as grin_main
 
 # Test Files: celegans_61mers.hgram fake1_61mers.hgram yeast_51mers.hgram fake2_61mers.hgram
 
@@ -12,26 +12,26 @@ class Test_find_start_repeat_kmers(unittest.TestCase):
 	def test_fake1(self):
 
 		with open("fake1_61mers.hgram", 'r') as f:
-			hist_dict = calc_gri_main.create_hist_dict(f)
-			self.assertTrue(73 <= calc_gri_main.find_start_repeat_kmers(hist_dict) <= 79)
+			hist_dict = grin_main.create_hist_dict(f)
+			self.assertTrue(73 <= grin_main.find_start_repeat_kmers(hist_dict) <= 79)
 
 	def test_fake2(self):
 
 		with open("fake2_61mers.hgram", 'r') as f:
-			hist_dict = calc_gri_main.create_hist_dict(f)
-			self.assertTrue(73 <= calc_gri_main.find_start_repeat_kmers(hist_dict) <= 79)
+			hist_dict = grin_main.create_hist_dict(f)
+			self.assertTrue(73 <= grin_main.find_start_repeat_kmers(hist_dict) <= 79)
 
 	def test_celegans(self):
 
 		with open("celegans_61mers.hgram", 'r') as f:
-			hist_dict = calc_gri_main.create_hist_dict(f)
-			self.assertTrue(27 <= calc_gri_main.find_start_repeat_kmers(hist_dict) <= 33)
+			hist_dict = grin_main.create_hist_dict(f)
+			self.assertTrue(27 <= grin_main.find_start_repeat_kmers(hist_dict) <= 33)
 
 	def test_yeast(self):
 
 		with open("yeast_51mers.hgram", 'r') as f:
-			hist_dict = calc_gri_main.create_hist_dict(f)
-			self.assertTrue(66 <= calc_gri_main.find_start_repeat_kmers(hist_dict) <= 72)
+			hist_dict = grin_main.create_hist_dict(f)
+			self.assertTrue(66 <= grin_main.find_start_repeat_kmers(hist_dict) <= 72)
 
 
 if __name__ == "__main__":
