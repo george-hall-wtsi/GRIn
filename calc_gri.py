2775e98460c15ad334fb0d20b90ec1511a7d0240
george-hall <georgehall017@gmail.com>
2016-03-24 14:52:39 +0000

    Added option for verbosity, and added GRI calculation in its own function

--- calc_gri.py
+++ calc_gri.py
@@ -30,26 +30,37 @@ def create_hist_dict(in_file):
 
 	return hist_dict
 
+def calculate_gri(hist_dict, verbosity):
+	start_repetitive_kmers = find_start_repeat_kmers(hist_dict)
+	if verbosity != 0:
+		print "Start of repetitive k-mers" , start_repetitive_kmers
+
+	total_number_kmers = sum((a * b) for (a, b) in hist_dict.items())
+	if verbosity != 0:
+		print "Total number of k-mers" , total_number_kmers
+
+	number_repetitive_kmers = 0
+	for (a, b) in hist_dict.items():
+		if (a >= start_repetitive_kmers):
+			number_repetitive_kmers += (a * b)
+
+	if verbosity != 0:
+		print "Number of repetitive k-mers" , number_repetitive_kmers
+
+	return ((1.0 * number_repetitive_kmers) / total_number_kmers)
 
 if __name__ == "__main__":
 
-	if len(sys.argv) != 2:
+	if len(sys.argv) < 2 or len(sys.argv) > 3:
 		print "usage: %s <file>" %(sys.argv[0])
 		sys.exit()
 
+	if len(sys.argv) == 3 and "-v" in sys.argv:
+		verbosity = 1
+	else:
+		verbosity = 0
+
 	with open(sys.argv[1], 'r') as f:
 		hist_dict = create_hist_dict(f)
-		start_repetitive_kmers = find_start_repeat_kmers(hist_dict)
-
-		print "Start of repetitive k-mers" , start_repetitive_kmers
-
-		total_number_kmers = sum((a * b) for (a, b) in hist_dict.items())
-		print "Total number of k-mers" , total_number_kmers
-
-		number_repetitive_kmers = 0
-		for (a, b) in hist_dict.items():
-			if (a >= start_repetitive_kmers):
-				number_repetitive_kmers += (a * b)
-
-		print "Number of repetitive k-mers" , number_repetitive_kmers
-
+		gri = calculate_gri(hist_dict, verbosity)
+		print gri
