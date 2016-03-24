543be4d40e5df162d8d042f3ce64b8bfad75aa2c
george-hall <georgehall017@gmail.com>
2016-03-24 11:50:44 +0000

    Stopped making user input parameters, started trying to do it automatically

--- calc_gri.py
+++ calc_gri.py
@@ -1,29 +1,53 @@
 import sys
+import scipy.signal
+import numpy as np
 
-if len(sys.argv) != 4:
-	print "usage: %s <start of first peak> <mid point first peak> <file>" %(sys.argv[0])
-	sys.exit()
+def find_start_repeat_kmers(hist_dict):
 
-# 'a' is section before mid point, 'b' is section after
-kmers_in_a = 0
-kmers_in_b = 0
-number_a = 0
-number_b = 0
+	window_size = 5
+	window = np.ones(int(window_size))/float(window_size)
+	moving_average = np.convolve(hist_dict.values(), window, 'same')
+	smoothed_data = dict(zip(hist_dict.keys(), [int(x) for x in moving_average]))
 
-start_peak_1 = int(sys.argv[1])
-midpoint = int(sys.argv[2])
+	order_num = 2
 
-with open(sys.argv[3], 'r') as f:
-	for line in f.readlines():
+	min_list = scipy.signal.argrelextrema(np.array(smoothed_data.values()), np.less_equal, 
+		order = order_num)[0].tolist()
+	max_list = scipy.signal.argrelextrema(np.array(smoothed_data.values()), np.greater_equal, 
+		order = order_num)[0].tolist()
+
+	#print "min_list:" , min_list
+	#print "max_list:" , max_list
+
+	return (2 * max_list[1]) - min_list[1]
+
+def create_hist_dict(in_file):
+	hist_dict = {}
+	for line in in_file.readlines():
 		splat = [int(x) for x in line.strip().split()]
-		if start_peak_1 <= splat[0] <= midpoint:
-			kmers_in_a += (splat[0] * splat[1])
-                        number_a += splat[1]
-		elif splat[0] > midpoint:
-			kmers_in_b += (splat[0] * splat[1])
-                        number_b += splat[1]
+		hist_dict[splat[0]] = splat[1]
 
-	print "k-mers before midpoint =" , kmers_in_a
-	print "k-mers after midpoint =" , kmers_in_b
-	print "GRI =" , ((1.0 * kmers_in_b) / kmers_in_a)
-        print "Other =" , ((1.0 * number_b) / number_a)
+	return hist_dict
+
+
+if __name__ == "__main__":
+
+	if len(sys.argv) != 2:
+		print "usage: %s <file>" %(sys.argv[0])
+		sys.exit()
+
+	with open(sys.argv[1], 'r') as f:
+		hist_dict = create_hist_dict(f)
+		start = find_start_repeat_kmers(hist_dict)[2]
+
+		print start
+
+		total_number_kmers = sum((a * b) for (a, b) in hist_dict.items())
+		print total_number_kmers
+
+		number_repetitive_kmers = 0
+		for (a, b) in hist_dict.items():
+			if (a >= start):
+				number_repetitive_kmers += (a * b)
+
+		print number_repetitive_kmers
