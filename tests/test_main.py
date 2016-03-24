9f1c0f74708c62eebfbdeb488da3a61fd7bfc59a
george-hall <georgehall017@gmail.com>
2016-03-24 14:14:04 +0000

    Changed expected values in start of repetitive k-mer estimation tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,19 +17,19 @@ class Test_find_start_repeat_kmers(unittest.TestCase):
 
 		with open("fake2_61mers.hgram", 'r') as f:
 			hist_dict = calc_gri_main.create_hist_dict(f)
-			self.assertTrue(70 <= calc_gri_main.find_start_repeat_kmers(hist_dict) <= 76)
+			self.assertTrue(73 <= calc_gri_main.find_start_repeat_kmers(hist_dict) <= 79)
 
 	def test_celegans(self):
 
 		with open("celegans_61mers.hgram", 'r') as f:
 			hist_dict = calc_gri_main.create_hist_dict(f)
-			self.assertTrue(23 <= calc_gri_main.find_start_repeat_kmers(hist_dict) <= 29)
+			self.assertTrue(27 <= calc_gri_main.find_start_repeat_kmers(hist_dict) <= 33)
 
 	def test_yeast(self):
 
 		with open("yeast_51mers.hgram", 'r') as f:
 			hist_dict = calc_gri_main.create_hist_dict(f)
-			self.assertTrue(64 <= calc_gri_main.find_start_repeat_kmers(hist_dict) <= 70)
+			self.assertTrue(66 <= calc_gri_main.find_start_repeat_kmers(hist_dict) <= 72)
 
 
 if __name__ == "__main__":
