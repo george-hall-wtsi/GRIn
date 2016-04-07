f3399127ade6e903dcf7eb48fd7711b3d9229232
george-hall <georgehall017@gmail.com>
2016-04-07 16:51:43 +0100

    Changed the test suite
    Added more files to the test suite, and stopped testing the same file
    with different k-mer sizes. I am currently only failing two tests out of
    11, and these are from files with really odd k-mer spectra.

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,9 +6,17 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import grin as grin_main
 
 # Test Files:
-# celegans_61mers.hgram fake1_61mers.hgram yeast_51mers.hgram fake2_61mers.hgram 
-# SRR534843_100X_15mers.hgram SRR534843_100X_31mers.hgram SRR534843_100X_51mers.hgram 
-# SRR534843_100X_71mers.hgram
+# celegans_61mers.hgram
+# fake1_61mers.hgram
+# yeast_51mers.hgram
+# fake2_61mers.hgram
+# SRR534843_100X_31mers.hgram
+# SRR403105_both.fastq.31mers.hist
+# SRR2880607_both_new100X.fastq.31mers.hist
+# SRR1649436_100X_both.fastq.31mers.hist
+# SRR1009262_both_100X.fastq.31mers.hist
+# ERR560569_100X_both.fastq.31mers.hist
+# yeast_sim.bfast.fastq.31mers.hist
 
 class Test_find_start_repeat_kmers(unittest.TestCase):
 
@@ -20,42 +28,57 @@ class Test_find_start_repeat_kmers(unittest.TestCase):
 	def test_fake1(self):
 		with open("fake1_61mers.hgram", 'r') as f:
 			hist_dict = grin_main.create_hist_dict(f)
-			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 73, 79)
+			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 71, 81)
 
 	def test_fake2(self):
 		with open("fake2_61mers.hgram", 'r') as f:
 			hist_dict = grin_main.create_hist_dict(f)
-			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 73, 79)
+			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 71, 81)
 
 	def test_celegans(self):
 		with open("celegans_61mers.hgram", 'r') as f:
 			hist_dict = grin_main.create_hist_dict(f)
-			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 27, 33)
-	
+			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 27, 37)
+
 	def test_yeast(self):
 		with open("yeast_51mers.hgram", 'r') as f:
 			hist_dict = grin_main.create_hist_dict(f)
-			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 66, 72)
-
-	def test_SRR534843_100X_15mers(self):
-		with open("SRR534843_100X_15mers.hgram", 'r') as f:
-			hist_dict = grin_main.create_hist_dict(f)
-			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 104, 110)
-
+			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 64, 74)
+	
 	def test_SRR534843_100X_31mers(self):
 		with open("SRR534843_100X_31mers.hgram", 'r') as f:
 			hist_dict = grin_main.create_hist_dict(f)
-			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 90, 96)
+			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 87, 97)
 
-	def test_SRR534843_100X_51mers(self):
-		with open("SRR534843_100X_51mers.hgram", 'r') as f:
+	def test_SRR403105_both(self):
+		with open("SRR403105_both.fastq.31mers.hist", 'r') as f:
 			hist_dict = grin_main.create_hist_dict(f)
-			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 67, 73)
+			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 65, 75)
 
-	def test_SRR534843_100X_71mers(self):
-		with open("SRR534843_100X_71mers.hgram", 'r') as f:
+	def test_SRR2880607_both_new100X(self):
+		with open("SRR2880607_both_new100X.fastq.31mers.hist", 'r') as f:
 			hist_dict = grin_main.create_hist_dict(f)
-			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 40, 46)
+			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 104, 114)
 
+	def test_SRR1649436_100X_both(self):
+		with open("SRR1649436_100X_both.fastq.31mers.hist", 'r') as f:
+			hist_dict = grin_main.create_hist_dict(f)
+			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 90, 100)
+
+	def test_SRR1009262_both_100X(self):
+		with open("SRR1009262_both_100X.fastq.31mers.hist", 'r') as f:
+			hist_dict = grin_main.create_hist_dict(f)
+			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 87, 97)
+
+	def test_ERR560569_100X_both(self):
+		with open("ERR560569_100X_both.fastq.31mers.hist", 'r') as f:
+			hist_dict = grin_main.create_hist_dict(f)
+			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 103, 113)
+
+	def test_yeast_sim(self):
+		with open("yeast_sim.bfast.fastq.31mers.hist", 'r') as f:
+			hist_dict = grin_main.create_hist_dict(f)
+			Test_find_start_repeat_kmers.run_find_start_repeat_kmers_test(self, hist_dict, 61, 71)
+	
 if __name__ == "__main__":
 	unittest.main()
