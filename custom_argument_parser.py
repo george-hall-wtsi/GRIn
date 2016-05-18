89f887e99e53e17eca2c4efce1f410f33e22e485
george-hall <gh10@sanger.ac.uk>
2016-05-18 10:00:26 +0100

    Linted
    Carried out a series of relatively small syntactical changes to please
    Pylint

--- custom_argument_parser.py
+++ custom_argument_parser.py
@@ -1,58 +1,79 @@
+
+"""
+Custom error messages for the parser used in GRIn, as I couldn't otherwise get
+the usage messages to do what I wanted
+"""
+
+
 import sys
 import argparse
 
 
 class CustomParser(argparse.ArgumentParser):
 
-	def generate_usage_message(self):
-		store_str = "usage: grin [-h] [-v] [-a] [-u upper-bound] [-i] "
-		store_str += "[-E single-error-cutoff] [-e error-cutoff [error-cutoff ...]] "
-		store_str += "[-c cutoff [cutoff ...]] -f file [file ...]\n"
+    """Subclass to get custom error messages"""
 
-		return store_str
+    def generate_usage_message(self):
+        """Custom usage message"""
+        store_str = "usage: grin [-h] [-v] [-a] [-u upper-bound] [-i] "
+        store_str += "[-E single-error-cutoff] "
+        store_str += "[-e error-cutoff [error-cutoff ...]] "
+        store_str += "[-c cutoff [cutoff ...]] -f file [file ...]\n"
 
-	def generate_help_message(self):
-		store_str = "\n"
-		store_str += self.generate_usage_message()
-		store_str += "\nrequired arguments:\n"
-		store_str += "\t-f, --file                 one or more input histogram files\n\n"
+        return store_str
 
-		store_str += "optional arguments:\n"
-		store_str += "\t-v, --verbose              print more output\n"
-		store_str += "\t-a, --analyzer             run kmerspectrumanalyzer and compute GRI "
-		store_str += "from that histogram instead\n" 
-		store_str += "\t-u, --upper-bound          upper bound after which k-mers do "
-		store_str += "not contribute to total or repetitive k-mer counts\n"
-		store_str += "\t-i, --ignore-error         estimate the erroneous k-mers and do "
-		store_str += "not include them in the total k-mer count\n"
-		store_str += "\t-e, --error-cutoffs        manual list of the end of the error peak "
-		store_str += "(one per file)\n"
-		store_str += "\t-E, --single-error-cutoff  manual cutoff of error peak, applied to "
-		store_str += "all files\n"
-		store_str += "\t-c, --cutoffs              list of manual cutoffs for start of "
-		store_str += "repetitive k-mers\n\n"
-		
-		store_str += "other arguments:\n"
-		store_str += "\t-h, --help                 print this message\n\n"
+    def generate_help_message(self):
+        """Custom help message"""
+        store_str = "\n"
+        store_str += self.generate_usage_message()
+        store_str += "\nrequired arguments:\n"
+        store_str += "\t-f, --file                 "
+        store_str += "one or more input histogram files\n\n"
 
-		store_str += "notes:\n"
-		store_str += "\t* if specified, the number of manually specified cutoffs must "
-		store_str += "equal the number of input files\n"
-		store_str += "\t* if specified, the number of manually specified error cutoffs must "
-		store_str += "equal the number of input files\n"
-		store_str += "\t* if specified, --upper-bound, --single-error-cutoff, and "
-		store_str += "--error-cutoffs must be integers\n"
-		store_str += "\t* --single-error-cutoff, --error-cutoffs, and --ignore-error are "
-		store_str += "mutually exclusive\n\n"
+        store_str += "optional arguments:\n"
+        store_str += "\t-v, --verbose              "
+        store_str += "print more output\n"
+        store_str += "\t-a, --analyzer             "
+        store_str += "run kmerspectrumanalyzer and compute GRI from that "
+        store_str += "histogram instead\n"
+        store_str += "\t-u, --upper-bound          "
+        store_str += "upper bound after which k-mers do not contribute to "
+        store_str += "total or repetitive k-mer counts\n"
+        store_str += "\t-i, --ignore-error         "
+        store_str += "estimate the erroneous k-mers and do not include them "
+        store_str += "in the total k-mer count\n"
+        store_str += "\t-e, --error-cutoffs        "
+        store_str += "manual list of the end of the error peak (one per "
+        store_str += "file)\n"
+        store_str += "\t-E, --single-error-cutoff  manual cutoff of error "
+        store_str += "peak, applied to all files\n"
+        store_str += "\t-c, --cutoffs              "
+        store_str += "list of manual cutoffs for start of repetitive "
+        store_str += "k-mers\n\n"
 
-		return store_str
+        store_str += "other arguments:\n"
+        store_str += "\t-h, --help                 print this message\n\n"
 
-	def print_usage(self, file = None):
-		if file is None:
-			file = sys.stdout
-		self._print_message(self.generate_usage_message(), file)
+        store_str += "notes:\n"
+        store_str += "\t* if specified, the number of manually specified "
+        store_str += "cutoffs must equal the number of input files\n"
+        store_str += "\t* if specified, the number of manually specified "
+        store_str += "error cutoffs must equal the number of input files\n"
+        store_str += "\t* if specified, --upper-bound, --single-error-cutoff, "
+        store_str += "and --error-cutoffs must be integers\n"
+        store_str += "\t* --single-error-cutoff, --error-cutoffs, and "
+        store_str += "--ignore-error are mutually exclusive\n\n"
 
-	def print_help(self, file = None):
-		if file is None:
-			file = sys.stdout
-		self._print_message(self.generate_help_message(), file)
+        return store_str
+
+    def print_usage(self, file=None):
+        """Print custom usage message"""
+        if file is None:
+            file = sys.stdout
+        self._print_message(self.generate_usage_message(), file)
+
+    def print_help(self, file=None):
+        """Print custom help message"""
+        if file is None:
+            file = sys.stdout
+        self._print_message(self.generate_help_message(), file)
