bebcee4fbaeb48fa764d3033937fe92ea1c911ab
george-hall <gh10@sanger.ac.uk>
2016-05-20 11:47:46 +0100

    Renamed estimation functions
    Tried to make it more obvious what is going on inside. Sorry for the
    unwieldy names...

--- grin.py
+++ grin.py
@@ -267,7 +267,7 @@ def set_cutoffs(indiv_cutoffs, single_cutoff, num_files):
         return [0 for _ in xrange(num_files)]
 
 
-def compute_error_cutoff(hist_dict, verbose, initial_error_cutoff):
+def estimate_error_cutoff_if_required(hist_dict, verbose, initial_error_cutoff):
 
     """
     Carry out error cutoff estimation as required.
@@ -287,7 +287,7 @@ def compute_error_cutoff(hist_dict, verbose, initial_error_cutoff):
         return initial_error_cutoff
 
 
-def compute_repeat_cutoff(hist_dict, verbose, initial_repeat_cutoff):
+def estimate_repeat_cutoff_if_required(hist_dict, verbose, initial_repeat_cutoff):
 
     """
     Carry out repeat cutoff estimation as necessary.
@@ -307,7 +307,7 @@ def compute_repeat_cutoff(hist_dict, verbose, initial_repeat_cutoff):
 
         return initial_repeat_cutoff
 
-def compute_upper_cutoff(hist_dict, verbose, initial_upper_cutoff):
+def estimate_upper_cutoff_if_required(hist_dict, verbose, initial_upper_cutoff):
 
     """
     Carry out upper cutoff estimation as necessary.
@@ -355,11 +355,11 @@ def process_histogram_file(file_name, verbose, in_error_cutoff,
 
         hist_dict = create_hist_dict(hist_file)
 
-        error_cutoff = compute_error_cutoff(hist_dict, verbose,
+        error_cutoff = estimate_error_cutoff_if_required(hist_dict, verbose,
                                             in_error_cutoff)
-        repeat_cutoff = compute_repeat_cutoff(hist_dict, verbose,
+        repeat_cutoff = estimate_repeat_cutoff_if_required(hist_dict, verbose,
                                               in_repeat_cutoff)
-        upper_cutoff = compute_upper_cutoff(hist_dict, verbose,
+        upper_cutoff = estimate_upper_cutoff_if_required(hist_dict, verbose,
                                             in_upper_cutoff)
 
         if check_cutoff_consistency(error_cutoff, repeat_cutoff,
